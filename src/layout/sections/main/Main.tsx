e67802877b4dde2977f6aa8b3280557323c21981
EgoshinVA <egoshinvit@gmail.com>
2024-09-11 11:57:58 +0700

    Add Header and icons

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div>
+            <div>
+                <span>Hi There</span>
+                <h2>I am Vitaliy Egoshin</h2>
+                <h1>A Web Developer.</h1>
+            </div>
+
+            <img src="" alt=""/>
+        </div>
+    );
+};
