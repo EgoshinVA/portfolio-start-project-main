54803c91bec65d41c92f30a07ba453d95c8b5f4b
EgoshinVA <egoshinvit@gmail.com>
2024-09-11 12:22:24 +0700

    Add Main layout

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi There</span>
-                <h2>I am Vitaliy Egoshin</h2>
-                <h1>A Web Developer.</h1>
-            </div>
+        <StyledMain>
+            <FlexWrapper align='center' justify='space-around'>
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Vitaliy Egoshin</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
 
-            <img src="" alt=""/>
-        </div>
+                <Photo src={photo} alt="photo"/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #d5c7c7;
+`
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+  
+`
+
+const Name = styled.h2`
+  
+`
