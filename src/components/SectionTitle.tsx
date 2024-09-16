707470238e60994ba49b7a2af27ed5bf1ee2cef6
EgoshinVA <egoshinvit@gmail.com>
2024-09-16 13:33:57 +0700

    Styling SectionTitle.tsx

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import {theme} from "../styles/Theme";
 
 type SectionTitlePropsType = {
     title: string
@@ -14,6 +15,23 @@ export const SectionTitle = (props: SectionTitlePropsType) => {
 };
 
 const StyledTitle = styled.h2`
-  color: #ffffff;
   text-align: center;
+  font-family: Josefin Sans, sans-serif;
+  font-size: 36px;
+  font-weight: 600;
+  letter-spacing: 5px;
+  position: relative;
+  margin-bottom: 90px;
+  
+  &::before{
+    position: absolute;
+    display: inline-block;
+    content: '';
+    width: 55px;
+    height: 1px;
+    background-color: ${theme.colors.accent};
+    bottom: -30px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `
