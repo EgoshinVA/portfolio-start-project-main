ce10e3229117659bd821f2a64e07e4f7140d4db5
EgoshinVA <egoshinvit@gmail.com>
2024-09-17 12:39:52 +0700

    Styling Slogan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
+import {Container} from "../../../components/Container";
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle title={'I Am Available For Freelance'}/>
-            <Button>Hire me</Button>
+            <Container>
+                <FlexWrapper align={'center'} direction={'column'}>
+                    <SectionTitle title={'I Am Available For Freelance'}/>
+                    <Button>Hire me</Button>
+                </FlexWrapper>
+            </Container>
         </StyledSlogan>
     );
 };
