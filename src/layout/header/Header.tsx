22ac7023929489f2864b7da13e40a4dc0a46ced3
EgoshinVA <egoshinvit@gmail.com>
2024-09-24 16:12:25 +0700

    Adaptive Header and menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,6 +4,7 @@ import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {HeaderMenu} from "./headerMenu/HeaderMenu";
+import {MobileMenu} from "./mobileMenu/MobileMenu";
 
 export const Header = () => {
     return (
@@ -12,6 +13,7 @@ export const Header = () => {
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo/>
                     <HeaderMenu items={['Home', 'Skills', 'Works', 'Testimony', 'Contact']}/>
+                    <MobileMenu items={['Home', 'Skills', 'Works', 'Testimony', 'Contact']}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
