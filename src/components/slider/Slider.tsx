683a3cb4c05573554b163321ccbfa7c695c2fc50
EgoshinVA <egoshinvit@gmail.com>
2024-09-30 17:31:15 +0700

    Add slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,23 +1,40 @@
+import {S} from './Slider_Styles';
 import React from 'react';
-import {FlexWrapper} from "../FlexWrapper";
-import { S } from './Slider_Styles';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import './../../styles/Slider.css'
 
-export const Slider: React.FC = () => {
+type SlidePropsType = {
+    text: string,
+    name: string
+}
+
+const Slide: React.FC<SlidePropsType> = (props) => {
     return (
-        <S.Slider>
-            <FlexWrapper>
-                <S.Slide>
-                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit.</S.Text>
-                    <S.Name>@ivan ivanow</S.Name>
-                </S.Slide>
-            </FlexWrapper>
-            <S.Pagination>
-                <span></span>
-                <span className={'active'}></span>
-                <span></span>
-            </S.Pagination>
-        </S.Slider>
-    );
-};
+        <S.Slide>
+            <S.Text>{props.text}</S.Text>
+            <S.Name>@{props.name}</S.Name>
+        </S.Slide>)
+}
+
+const items = [
+    <Slide text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n' +
+        '                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing\n' +
+        '                elit.'} name={'ivan ivanow'}/>,
+    <Slide text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n' +
+        '                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing\n' +
+        '                elit.'} name={'viktor kvalow'}/>,
+    <Slide text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n' +
+        '                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing\n' +
+        '                elit.'} name={'unknown user'}/>
+];
+
+export const Slider = () => (
+    <S.Slider>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+            disableButtonsControls
+        />
+    </S.Slider>
+);
--- src/components/slider/Slider_Styles.ts
+++ src/components/slider/Slider_Styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import {theme} from "../../styles/Theme";
 
 const Slider = styled.div`
   max-width: 500px;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -22,29 +22,10 @@ const Name = styled.span`
   margin: 22px 0 32px;
   display: inline-block;
 `
-const Pagination = styled.span`
-  span {
-    display: inline-block;
-    width: 7px;
-    height: 7px;
-    border-radius: 20px;
-    background-color: rgba(255, 255, 255, 0.5);
-
-      & + span {
-      margin-left: 5px;
-    }
-    
-    &.active {
-      background-color: ${theme.colors.accent};
-      width: 20px;
-    }
-  }
-`
 
 export const S = {
     Slider,
     Slide,
     Text,
-    Name,
-    Pagination
+    Name
 }
